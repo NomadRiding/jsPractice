2bbd75730107d08631be2dc71bd7422e0e82a0d0
NomadRiding <nomadriding@gmail.com>
2020-04-16 08:42:29 -0400

    Battleship Req.

--- snipets.js
+++ snipets.js
@@ -1,1 +1,13 @@
 document.getElementById('welcomeTitle').innerHTML = 'My JavaScript practice white board';
+
+
+/*
+------ TASK ------
+Create a 1-D grid battleship game.
+- 1 x 7 grid
+- 1 ship 3 spaces
+- Hit or Miss alert
+- Ship sunk alert
+- Game stats
+*/
+
