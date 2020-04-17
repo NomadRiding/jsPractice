40d66ad6f11784fd22596dc7e2af55134cbfed05
NomadRiding <nomadriding@gmail.com>
2020-04-17 18:12:25 -0400

    create best solution function

--- snipets.js
+++ snipets.js
@@ -92,6 +92,10 @@ let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
   34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
   46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
+let costs = [.25,.27,.25,.25,.25,.25,.33,.31,.25,.29,.27,.22,
+  .31,.25,.25,.33,.21,.25,.25,.25,.28,.25,.24,.22,.20,.25,.30,
+  .25,.24,.25,.25,.25,.27,.25,.26,.29];
+
 function printAndGetHighScore(scores){
   let highScore = 0;
   let output = 0;
@@ -118,4 +122,20 @@ function getBestResults(scores,highScore){
 }
 
 let bestSolution = getBestResults(scores,highScore);
-document.write("Solutions with the highest score: " + "<b>" + bestSolution + "<b>" + "<br>");
+document.write("Solutions with the highest score: " + "<b>" + bestSolution + "</b>" + "<br>");
+
+function getMostCostEffectiveSolution(scores,costs,highScore){
+  let cost = 100;
+  let index;
+  for(let i = 0; i < scores.length; i++){
+    if(scores[i] == highScore){
+      if (cost > costs[i]) {
+        index = i;
+        cost = costs[i]
+      }
+    }
+  } return index;
+}
+
+let mostCostEffective = getMostCostEffectiveSolution(scores,costs,highScore);
+document.write("Bubble solution #" + "<b>" + mostCostEffective + "</b>" + " is the most cost effective." + "<br>");
